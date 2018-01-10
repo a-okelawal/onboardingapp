72bf1d8a34ef6d153a1e7e046dcb79a2b1169f48
Saint-lawal <abiso.lawal@gmail.com>
2018-01-10 13:58:44 +0100

    - Add bycrypt and user save logic

---
+++ app/controllers/UserController.js
@@ -0,0 +1,49 @@
+import bcrypt, { genSalt } from 'bcrypt';
+import User from '../models/User';
+
+export default class UserController {
+  /**
+   * Logic for creating a user
+   * @param {*} req 
+   * @param {*} res 
+   */
+  static create(req, res) {
+    const body = req.body;
+
+    User.findOne({
+      username: body.username
+    }, (err, user) => {
+      if (err) {
+        res.status(500).send(err);
+      } else if (user) {
+        res.status(409).send({ message: 'User already exists.' });
+      } else {
+        const salt = 10;
+        bcrypt.genSalt(salt, (err, genSalt) => {
+          if (err) {
+            res.status(500).send(err);
+          } else {
+            bcrypt.hash(body.password, (err, hash) => {
+              if (err) {
+                res.status(500).send(err);
+              }
+              user = new User({
+                username: body.username,
+                password: hash
+              });
+
+              user.save((err) => {
+                if (err) {
+                  res.status(500).send(err);
+                } else {
+                  //TODO: Also save employee details is employee
+                  res.status(201).send({ message: `User ${user.username} has been created successfully.` });
+                }
+              });
+            });
+          }
+        });
+      }
+    });
+  }
+}
