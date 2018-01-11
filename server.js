b8e438d69936f4f64f3554049f2cd88927925e49
Saint-lawal <abiso.lawal@gmail.com>
2018-01-11 12:54:18 +0100

    - Reflect config changes in server

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 import Routes from './app/routes/routes';
-import Config from './config/Config';
+import config from './config/Config';
 
 const app = express();
 const router = express.Router();
-const config = Config.config();
-
-dotenv.config();
 
 // Connect to db once config is set
-mongoose.connect(Config.config().db);
+mongoose.connect(config.db);
 
-const port = process.env.PORT || 3000;
+const port = config.port
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
