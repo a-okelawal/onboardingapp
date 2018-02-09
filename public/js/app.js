f76dc317aa5037ff120c30a04c7484a135f42d53
Abisoye Oke-lawal <abisoye.oke-lawal@andela.com>
2018-02-09 03:37:50 +0100

    Revert "Feature: Change Employee"

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-angular.module('employapp', ['ngRoute', 'ngCookies', 'appRoutes', 'angular-toasty', 'AuthCtrl', 'AuthSrv', 'BaseCtrl', 'CookieSrv', 'DepartmentCtrl', 'DepartmentSrv', 'EmployeeCtrl', 'EmployeeListCtrl', 'EmployeeSrv', 'HomeCtrl', 'SharedSrv', 'TaskCtrl', 'TaskSrv', 'TaskListCtrl']);
+angular.module('employapp', ['ngRoute', 'ngCookies', 'appRoutes', 'angular-toasty', 'AuthCtrl', 'AuthSrv', 'BaseCtrl', 'CookieSrv', 'DepartmentCtrl', 'DepartmentSrv', 'EmployeeCtrl', 'EmployeeSrv', 'HomeCtrl', 'SharedSrv', 'TaskCtrl', 'TaskSrv', 'TaskListCtrl']);
--- public/js/services/EmployeeSrv.js
+++ public/js/services/EmployeeSrv.js
@@ -1,14 +1,7 @@
 angular.module('EmployeeSrv', ['CookieSrv']).factory('EmployeeService', ['$http', 'CookieService', function($http, CookieService) {
   return {
     read: function() {
-      return $http.get('http://localhost:3001/api/v1/employees', {
-        headers: {
-          'x-access-token': CookieService.getToken()
-        }
-      });
-    },
-    update: function(details, id) {
-      return $http.put(`http://localhost:3001/api/v1/employees/${id}`, details, {
+      return $http.get('http://localhost:3001/api/v1/employee', {
         headers: {
           'x-access-token': CookieService.getToken()
         }
